8cf9420bb961064d433a80d08e8dd7eb057aedbf
George Chen <gchen@zoiclabs.com>
2023-01-03 14:12:02 -0800

    socket set up wip

---
+++ app/src/routes/Root.tsx
@@ -0,0 +1,10 @@
+export default function Root() {
+  return (
+    <div>
+      Welcome to Bonikers
+      <button>START NEW GAME</button>
+      <button>Game</button>
+      <button>UserId</button>
+    </div>
+  )
+}
